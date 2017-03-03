f6237d5a711be0d7e0f08aa99ebff3a5805ea7ce
Robert <robert.gol.jr@gmail.com>
2017-03-03 14:05:53 -0300

    Removing firebase

--- js/index.js
+++ js/index.js
@@ -1,16 +1,5 @@
 'use strict';
 
-//
-// // Initialize Firebase
-// var config = {
-//   apiKey: "AIzaSyBEyLvk5uNbqE3DCVdOeFaFyUG6W5ez_X8",
-//   authDomain: "eliane-imoveis.firebaseapp.com",
-//   databaseURL: "https://eliane-imoveis.firebaseio.com",
-//   storageBucket: "eliane-imoveis.appspot.com",
-//   messagingSenderId: "51582427423"
-// };
-// firebase.initializeApp(config);
-
 
 window.addEventListener('hashchange', router.run);
 window.addEventListener('load', router.run);
--- js/router.js
+++ js/router.js
@@ -6,26 +6,23 @@
 var router = (function () {
   var el = document.getElementById('content');
   var markdown = new showdown.Converter();
+
   markdown.setFlavor('github');
   markdown.setOption('requireSpaceBeforeHeadingText', true);
 
   return {
     run: function () {
-      var url = location.pathname.slice(1) || '/';
-      if (url === '/')
-        request.page('index').then(function (e) {
-          let page = e.target.response;
-          el.innerHTML = markdown.makeHtml(page);;
-        });
-      else
-        request.page(url).then(function (e) {
-          let page = e.target.response;
-          let status = e.target.status;
-          if (page && status === 200)
-            el.innerHTML = markdown.makeHtml(template.run(url, page));
-          else
-            el.innerHTML = markdown.makeHtml('Página não encontrada');
-        });
+      var url = location.pathname.slice(1) || 'index';
+
+      request.page(url).then(function (e) {
+        let page = e.target.response;
+        let status = e.target.status;
+        
+        if (page && status === 200)
+          el.innerHTML = markdown.makeHtml(template.run(url, page));
+        else
+          el.innerHTML = markdown.makeHtml('Página não encontrada');
+      });
     }
   }
 })()
