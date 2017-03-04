d22fa5750e3b4a370a7eb0b4c323f96d7e53baf9
Robert <robert.gol.jr@gmail.com>
2017-03-04 10:53:16 -0300

    Request Module: get imovel by id & get imoveis list

--- js/request.js
+++ js/request.js
@@ -5,12 +5,30 @@
 //
 var request = (function () {
   return {
-    page: function (page) {
+    template: function (page) {
       return new Promise(function (resolve, reject) {
         var oReq = new XMLHttpRequest();
         oReq.addEventListener('load', resolve);
         oReq.addEventListener('error', reject);
         oReq.open('GET', '/pages/'+page+'.md');
+        oReq.send();
+      });
+    },
+    imovel: function (id) {
+      return new Promise(function (resolve, reject) {
+        var oReq = new XMLHttpRequest();
+        oReq.addEventListener('load', resolve);
+        oReq.addEventListener('error', reject);
+        oReq.open('GET', '/imoveis/'+id+'/imovel.json');
+        oReq.send();
+      });
+    },
+    imoveisList: function () {
+      return new Promise(function (resolve, reject) {
+        var oReq = new XMLHttpRequest();
+        oReq.addEventListener('load', resolve);
+        oReq.addEventListener('error', reject);
+        oReq.open('GET', '/imoveis/imoveis.json');
         oReq.send();
       });
     }
